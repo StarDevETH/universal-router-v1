3eb48ee3951d5e9e98b49940df220d6e7a6f7b9b
StarDevETH <apestarminer@gmail.com>
2025-09-08 03:19:19 -0500

    added lib/

--- contracts/base/Dispatcher.sol
+++ contracts/base/Dispatcher.sol
@@ -8,7 +8,7 @@ import {Payments} from '../modules/Payments.sol';
 import {Commands} from '../libraries/Commands.sol';
 import {LockAndMsgSender} from './LockAndMsgSender.sol';
 import {ERC20} from 'solmate/src/tokens/ERC20.sol';
-import {IAllowanceTransfer} from 'permit2/src/interfaces/IAllowanceTransfer.sol';
+import {IAllowanceTransfer} from 'lib/permit2/src/interfaces/IAllowanceTransfer.sol';
 
 /// @title Decodes and Executes Commands (ERC20-only)
 /// @notice Called by the UniversalRouter to efficiently decode and execute a single command
--- contracts/base/RouterImmutables.sol
+++ contracts/base/RouterImmutables.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
-import {IAllowanceTransfer} from 'permit2/src/interfaces/IAllowanceTransfer.sol';
+import {IAllowanceTransfer} from 'lib/permit2/src/interfaces/IAllowanceTransfer.sol';
 import {IWETH9} from '../interfaces/external/IWETH9.sol';
 
 struct RouterParameters {
